cf8b42bf9d424b0597a132f6d44a8b38acedc599
shahaf <shahaf.sudai@gmail.com>
2024-01-21 13:53:22 +0200

    Writing main- If no bugs will be found, this is our final commit.

--- main.c
+++ main.c
@@ -3,19 +3,59 @@
 #define FALSE 0
 #define TRUE 1
 int main(){
-    int a=FALSE;
-    int b=FALSE;
-    int c=FALSE;
-    int d=FALSE;
-    for (int i =400;i<450;i++){
-        a=isStrong(i);
-        b=isPrime(i);
-        c=isAmstrong(i);
-        d=isPalindrome(i);
-        printf("%d is strong: %d\n",i,a);
-        printf("%d is prime: %d\n",i,b);
-        printf("%d is Amstrong: %d\n",i,c);
-        printf("%d is Palindrome: %d\n",i,d);
-        printf("\n");
+    //initiolize all parameters
+    int condition=FALSE;
+    int num1=-1;
+    int num2=-1;
+    int max=0;
+    int min=0;
+    // take input numbers, if negative take again 
+    while(num1<0){
+        scanf("%d", &num1);
     }
+    while(num2<0){
+        scanf("%d", &num2);
+    }
+    //find max and min
+    if(num1<=num2){
+        max=num2;
+        min=num1;
+    }
+    else{
+        max=num1;
+        min=num2; 
+    }
+    //print results
+    for (int i =min;i<max+1;i++){
+        condition=isPrime(i);
+        if(condition==1){
+            printf("%d ",i);
+        }
+    }
+    printf("\n\n");
+
+    for (int i =min;i<max+1;i++){
+        condition=isAmstrong(i);
+        if(condition==1){
+            printf("%d ",i);
+        }
+    }
+    printf("\n\n");
+
+    for (int i =min;i<max+1;i++){
+        condition=isStrong(i);
+        if(condition==1){
+            printf("%d ",i);
+        }
+    }
+    printf("\n\n");
+
+    for (int i =min;i<max+1;i++){
+        condition=isPalindrome(i);
+        if(condition==1){
+            printf("%d ",i);
+        }
+    }
+    printf("\n");
+
 }
